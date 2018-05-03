c9d94254c22b9267e80b44931252f8e002680c5c
安 卢 <laa6203@163.com>
2018-05-03 21:58:05 +0800

    m xdm_i.m

--- matlab/xdm_i.m
+++ matlab/xdm_i.m
@@ -5,18 +5,20 @@
 clear all
 clc
 
+%1. global varialbe
 com_num = 'COM13';
-total_sec = 10;
+total_sec = 30;
 
 
-%1. file name
+%2. file name
 tt = clock;
 fn = strcat('d',num2str(tt(1)-2000),num2str(tt(2)),num2str(tt(3)),'_',num2str(tt(4)),num2str(tt(5)),num2str(round(tt(6))),'.dat');
+disp(strcat('file name :', fn));
 fid = fopen(fn,'a+');
 clear tt;
 
 
-%2. open COM
+%3. open COM
 data = zeros(8000,1);
 s13 =  serial(com_num, 'BaudRate', 115200);
 set(s13,'InputBufferSize',50000);
@@ -26,21 +28,25 @@ if(s13.BytesAvailable >0)
 	data = fread(s13,s13.BytesAvailable,'uint8');
 end
 
-%3. save data
+%4. save data
 sec_index = 0;
 while(1)
     if(s13.BytesAvailable > 8000)
-      data = fread(s13,8000,'uint8');
-      fwrite(fid,data,'uint8');
-			sec_index = sec_index + 1;
-			if(sec_index == total_sec)
-				break;
+        data = fread(s13,8000,'uint8');
+        fwrite(fid,data,'uint8');
+        sec_index = sec_index + 1;
+        disp(strcat('sec_index : ',int2str(sec_index),'/',int2str(total_sec)));
+        if(sec_index == total_sec)
+			break;
+        end
     end
 end
 
 
-%4.close file
+%5.close file
 clear sec_index;
 clear total_sec;
 fclose(s13);
 fclose(fid);
+disp('END of uart data file save');
+
