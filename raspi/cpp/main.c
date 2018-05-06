1b1d25f319277c5645c68e96f0417b8572bdefb4
laa6202 <laa6202@163.com>
2018-05-07 02:03:33 +0800

    m

--- raspi/cpp/main.c
+++ raspi/cpp/main.c
@@ -1,6 +1,22 @@
+#include <wiringPi.h>
+
 #include <stdio.h>
+#include <commu.h>
+
+
 
 int main(int agrc,char *argv[]){
 	printf("mpp\n");	
+
+	wiringPiSetup();
+	I2C_Init();
+
+	for(int i=0;i<1;i++){
+		I2C_Test();
+		delay(300);
+		printf("Main i=%d\n",i);	
+	}	
 	return 0;
 }
+
+
