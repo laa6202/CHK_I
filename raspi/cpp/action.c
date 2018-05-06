12fac55be3110230c01e10cd77fbfd1092fb115e
laa6202 <laa6202@163.com>
2018-05-07 03:38:46 +0800

    m types

--- raspi/cpp/action.c
+++ raspi/cpp/action.c
@@ -1,10 +1,18 @@
 
 #include <stdio.h>
+#include <string.h>
 
 #include <action.h>
 #include <commu.h>
+#include <types.h>
+
+#define LEN_BUF 32
+
+U8 recBuf[LEN_BUF];
+
 
 int AppInit(){
+	memset(recBuf,0,LEN_BUF*sizeof(U8));
 	I2C_Init();
 	
 	return 0;
@@ -12,9 +20,9 @@ int AppInit(){
 
 
 int AppMain(){
-
-	GetTPKG();
-	ShowTPKG();
+	GetTPKG(recBuf);
+	ShowTPKG(recBuf);
+	
 	return 0;
 }
 
--- raspi/cpp/inc/types.h
+++ raspi/cpp/inc/types.h
@@ -1,6 +1,10 @@
 #ifndef __TYPES_H
 #define __TYPES_H
 
+typedef unsigned char 	U8;
+typedef unsigned short 	U16;
+typedef unsigned int 		U32;
+
 typedef struct {
 	int rot;
 	int t1;
