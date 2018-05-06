58b6e79e113b65858d75b1ab503ab3f90d12bfd7
laa6202 <laa6202@163.com>
2018-05-07 02:58:42 +0800

    a

--- raspi/cpp/inc/types.h
+++ raspi/cpp/inc/types.h
@@ -1,6 +1,12 @@
 #ifndef __TYPES_H
 #define __TYPES_H
 
+typedef struct {
+	int rot;
+	int t1;
+	int oil_p;
+}	MESS, *pMESS;
+
 
 
 #endif
--- raspi/cpp/main.c
+++ raspi/cpp/main.c
@@ -6,15 +6,16 @@
 
 
 int main(int agrc,char *argv[]){
-	printf("mpp\n");	
+	printf("XMPP the Low APP of XDM-I\n");	
 
 	wiringPiSetup();
 	I2C_Init();
 
 	for(int i=0;i<1;i++){
-		I2C_Test();
+	GetTPKG();
+//		I2C_Test();
 		delay(300);
-		printf("Main i=%d\n",i);	
+//		printf("Main i=%d\n",i);	
 	}	
 	return 0;
 }
