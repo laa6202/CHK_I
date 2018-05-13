512dab19db3ecaa937d6f0f279be0ca322867301
安 卢 <laa6203@163.com>
2018-05-13 21:51:29 +0800

    m

--- mcu/rmcu/code/src/commu.c
+++ mcu/rmcu/code/src/commu.c
@@ -7,15 +7,18 @@
 #include "types.h"
 #include "commu.h"
 
-#define LEN_U1_BUF 64
+#define LEN_U1_BUF 32
 
 
 
 int recU1Number;
 U8 recU1Buf[LEN_U1_BUF];
 int recU1Index;
-U8 recI2C_Data;
+U8 recI2C_RecData;
+U8 recI2C_SendData;
 U8 temp;
+int recI2C_Number;
+int irqI2C_Number;
 
 int U1_Init(){
 	recU1Number = 0;
@@ -47,7 +50,7 @@ int U1RecData(){
 	else{
 		recU1Buf[recU1Index] = recU1Data;
 		recU1Index++;
-		if(recU1Index == 64)	recU1Index = 0;	
+		if(recU1Index == LEN_U1_BUF)	recU1Index = 0;	
 		return 0;
 	}
 }
@@ -61,15 +64,25 @@ int U1_IRQ_END(){
 
 int I2C_Init(){
 	temp = 0;
+	recI2C_Number = 0;
+	recI2C_RecData = 0;
+	recI2C_SendData = 0;
+	irqI2C_Number = 0;
 	I2C1->CR2 = I2C1->CR2 | I2C_CR2_ITEVTEN;
-	I2C1->CR1 = I2C1->CR1 | I2C_CR1_ACK;
+	I2C1->CR1 = I2C1->CR1 | I2C_CR1_ACK | I2C_CR1_NOSTRETCH; 
+
 	return 0;
 }
 
 
 int I2C_EVIRQ(){
-	recI2C_Data = I2C1->DR;
-	I2C1->DR = 0x30+temp++;
+	if((I2C1->SR1 & I2C_SR1_RXNE) == I2C_SR1_RXNE){
+		recI2C_RecData = I2C1->DR;
+		recI2C_SendData = recU1Buf[recI2C_RecData];
+		recI2C_Number++;
+	}
+	I2C1->DR = recI2C_SendData;
+	irqI2C_Number++;
 	return 0;
 }
 
