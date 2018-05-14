d315da5d4d7dc90decb95f4192ae0e05e0a23479
laa6202 <laa6202@163.com>
2018-05-14 22:11:32 +0800

    update types.h

--- raspi/cpp/inc/types.h
+++ raspi/cpp/inc/types.h
@@ -8,14 +8,20 @@ typedef unsigned int 		U32;
 typedef struct {
 	int rot;
 	int t1;
+	int t2;
+	int tCore;
 	int oil_p;
+	int cntM;
+	int cntX;
 	int isM;
 	int error;
 }	REC1, *pREC1;
 
 
 typedef struct {
-	int sum_x;
+	int rot;
+	int t1;
+	int sumX;
 }	REC2, *pREC2;
 
 
