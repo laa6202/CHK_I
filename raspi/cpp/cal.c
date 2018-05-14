1c39acb044a0b0b43ee0d04d83e07141ad2bbc4c
laa6202 <laa6202@163.com>
2018-05-14 21:37:46 +0800

    m CalRot

--- raspi/cpp/cal.c
+++ raspi/cpp/cal.c
@@ -6,15 +6,16 @@
 #include <types.h>
 
 int CalRec1(pREC1 rec,const U8 * recBuf){
-
+	int rot = CalRot(recBuf);
 
 	return 0;
 }
 
 
 int CalRot(const U8 * recBuf){
-
-
+	int rot0 = *(recBuf+4) | (*(recBuf+5) << 8);
+	int rot = rot0 * 50;
+	printf("rot0 = %d\t rot = %d\n",rot0,rot);
 	return 0;
 }
 
