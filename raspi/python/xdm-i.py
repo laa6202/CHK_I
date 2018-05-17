555e1654fab37c8efb599c2440deba06540f11f3
laa6202 <laa6202@163.com>
2018-05-17 23:15:07 +0800

    add rec2.dat

--- raspi/python/xdm-i.py
+++ raspi/python/xdm-i.py
@@ -3,6 +3,7 @@ import datetime
 import random
 import pygame,sys
 from pygame.locals import *
+import os
 
 FPS = 2
 WINWIDTH=1200
@@ -13,7 +14,10 @@ FNSYSOK = 'sysOK.png'
 FNSYSWARNING = 'sysWarning.png'
 FNSYSERROR  = 'sysError.png'
 FNRPM = 'rpm.png'
+FNOIL = 'oil.png'
 FNTEM = 'tem.png'
+FNXXX = 'xxx.png'
+
 
 
 #           R   G   B
@@ -28,9 +32,14 @@ YELLOW	= (255,255,  0)
 BGCOLOR = WHITE
 
 def main():
-    global FPSCLOCK,DIS
-    global stSYS
+    global FPSCLOCK
+#    global DIS
+    fnValue = 'rec2.dat'
     stSYS = 0
+    rot = 3200
+    tem = 85.07
+    oil = 1234.56
+    xxx = 'AA'
     stDirPic = PicDirInit()
     DIS=DisInit()
     FPSCLOCK = pygame.time.Clock()
@@ -43,20 +52,43 @@ def main():
                 pygame.quit()
                 sys.exit()
         DIS.fill(WHITE)
+        GetValue(fnValue)
         ShowAll(stDirPic)
         pygame.display.update()
         FPSCLOCK.tick(FPS)
 
 
+def GetValue(fnValue) :
+    global stSYS
+    global rot
+    global tem
+    global oil
+    global xxx
+    stSYS = 0
+    rot = 3200
+    tem = 85.07
+    oil = 1234.56
+    xxx = 'AA'
+    fd = open(fnValue,'r')
+    s = fd.readline()
+    s = fd.readline()
+    s = fd.readline()
+    s = fd.readline()
+    print(str(float(s)))
+    fd.close()
+    
+
+
 def ShowAll(stDirPic):
     ShowLogo(stDirPic,(0,0))
-    ShowTitle(stDirPic,(200,0))
+    ShowTitle(stDirPic,(300,0))
     pygame.draw.line(DIS,NAVYBLUE,(10,110),(WINWIDTH-10,110),3)
     ShowSysStatus(stDirPic,stSYS,(0,120))
    # ShowTime(BLUE,(500,128))
-    ShowRPM(stDirPic,(0,160))
-    ShowTEM(stDirPic,(0,360))
-
+    ShowRPM(stDirPic,rot,(0,160))
+    ShowOil(stDirPic,oil,(0,260))
+    ShowTEM(stDirPic,tem,(0,360))
+    ShowXXX(stDirPic,xxx,(0,460))
 
 
 def PicDirInit():
@@ -71,12 +103,14 @@ def ShowLogo(stDirPic,pos):
     #print(fnLogo,type(fnLogo),pos)
     surLogoSrc = pygame.image.load(fnLogo)
     surLogoDst = pygame.transform.scale(surLogoSrc,(200,100))
+    global DIS
     DIS.blit(surLogoDst,pos)    
 
 
 def ShowTitle(stDirPic,pos):
     fnTitle = stDirPic + '/' + FNTITLE
     surTitle = pygame.image.load(fnTitle)
+    global DIS
     DIS.blit(surTitle,pos)
     #print(objFont)
 
@@ -90,6 +124,7 @@ def ShowSysStatus(stDirPic,flag,pos):
         fnSysStatus = stDirPic + '/' + FNSYSERROR
 #    print(fnSysStatus)
     surSysStatus = pygame.image.load(fnSysStatus)
+    global DIS
     DIS.blit(surSysStatus,pos)
 
 
@@ -100,35 +135,73 @@ def ShowTime(color,pos):
     objFont = pygame.font.SysFont(stFont,32,False)
 #    print(objFont.size(stNow))
     surTime = objFont.render(stNow,True,color)
+    global DIS
     DIS.blit(surTime,pos)
 
 
-def ShowRPM(stDirPic,pos):
+def ShowRPM(stDirPic,rpm,pos):
     fnRPM = stDirPic + '/' + FNRPM
     surRPM = pygame.image.load(fnRPM)
+    global DIS
     DIS.blit(surRPM,pos)
 
-    rpm = random.randrange(400,3800,100)
     stFont = pygame.font.get_default_font()
     objFont = pygame.font.SysFont(stFont,100,False) 
     surRPM = objFont.render(str(rpm),True,YELLOW)
     pos2 = list(pos)
     pos2[0] += 420
     pos2[1] += 0
-    print(pos)
     DIS.blit(surRPM,pos2)
 
 
-def ShowTEM(stDirPic,pos):
+def ShowOil(stDirPic,oil,pos) :
+    fnOIL = stDirPic + '/' +FNOIL
+    surOIL = pygame.image.load(fnOIL)
+    global DIS
+    DIS.blit(surOIL,pos)
+
+    stFont = pygame.font.get_default_font()
+    objFont = pygame.font.SysFont(stFont,100,False)
+    surOIL = objFont.render(str(oil),True,GREEN)
+    pos2 = list(pos)
+    pos2[0] += 420
+    pos2[1] += 0
+    DIS.blit(surOIL,pos2)
+    
+
+
+def ShowTEM(stDirPic,tem,pos):
     fnTEM = stDirPic + '/' +FNTEM
     surTEM = pygame.image.load(fnTEM)
+    global DIS
     DIS.blit(surTEM,pos)
+
+    stFont = pygame.font.get_default_font()
+    objFont = pygame.font.SysFont(stFont,100,False)
+    if tem > 69 :
+        color = RED
+    elif tem > 60 :
+        color = YELLOW
+    else :
+        color = GREEN
+    surTEM = objFont.render(str(tem),True,color)
+    pos2 = list(pos)
+    pos2[0] += 420
+    pos2[1] += 0
+    DIS.blit(surTEM,pos2)
     
+
+def ShowXXX(stDirPic,xxx,pos) :
+    fnXXX = stDirPic + '/' + FNXXX
+    surXXX = pygame.image.load(fnXXX)
+    global DIS
+    DIS.blit(surXXX,pos)
 
 
 def DisInit():
     pygame.init()
     #DIS = pygame.display.set_mode((WINWIDTH,WINHEIGHT),pygame.FULLSCREEN|pygame.HWSURFACE)
+    global DIS
     DIS = pygame.display.set_mode((WINWIDTH,WINHEIGHT))
     pygame.display.set_caption("XDM_I")
     DIS.fill(BGCOLOR)
